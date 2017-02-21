3bdcc8f76919d92c9250480e0ee8bc4a441212f0
guiyi <zouhdai@gmail.com>
2017-02-21 18:13:29 +0800

    555.php upload

---
+++ 555.php
@@ -0,0 +1,19 @@
+<?php
+
+$num =6;
+for($i=1;$i<=$num;$i++){
+    for($j=1;$j<=($num-$i);$j++){
+        echo $num-$i;
+    }
+    echo '<BR/>';
+}
+
+
+$num1= 5;
+for($i=5;$i>0;$i--){
+    for($j=5;$j>0;$j--){
+        echo $num1-$i;
+    }
+    echo '<BR/>';
+}
+?>
